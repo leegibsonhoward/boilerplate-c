5573cc8bba631fa204ede8bdd3704f1d36eea30f
Lee Howard <leegibsonhoward@gmail.com>
2023-11-11 22:28:04 -0800

    add main.c
    - simple countdown program using for loop

---
+++ main.c
@@ -0,0 +1,16 @@
+// main.c
+
+#include <stdio.h>
+
+int main() {
+
+  printf("Countdown has begun...\n");
+
+  for (int i = 10; i > 0; i--) {
+    printf(" %d \n", i);
+  }
+
+  printf("Blast off!!!\n");
+
+  return 0;
+}
